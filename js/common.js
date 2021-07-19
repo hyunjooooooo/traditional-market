cacd0fe0fbfaf1a5de481cec74bd812969affc45
hyunju <86402085+hyunjooooooo@users.noreply.github.com>
2021-07-19 15:14:50 +0900

    v-1

--- js/common.js
+++ js/common.js
@@ -3,8 +3,7 @@ window.addEventListener('DOMContentLoaded',function(){
 var menu = document.querySelector('nav div')
 var mBtn = document.querySelector('nav a')
 
-
-
+    
     mBtn.addEventListener('click',function(){
         if(menu.className !='active'){
             menu.className='active'

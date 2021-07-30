683169db7152813dea3a0751ece9826ae52e4cfd
hyunju <86402085+hyunjooooooo@users.noreply.github.com>
2021-07-30 13:18:13 +0900

    v-2

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 window.addEventListener('DOMContentLoaded',function(){
 
-  $(".main_banner").slick({
-    arrows:false, //화살표 사라짐 
-    dots: true, //인디게이터 
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay:true,
-    fade:true,
-    cssEase:'linear',
-    autoplaySpeed:2000 //자동 넘김
+    $(".main_banner").slick({
+      arrows:false, //화살표 사라짐 
+      dots: true, //인디게이터 
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay:true,
+      fade:true,
+      cssEase:'linear',
+      autoplaySpeed:2000 //자동 넘김
+    });
   });
-});
+  
--- js/search.js
+++ js/search.js
@@ -52,3 +52,11 @@ function loadCityChild(){
 }
 
 loadCity();
+
+
+var mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+};
+
+var map = new naver.maps.Map('map', mapOptions);
